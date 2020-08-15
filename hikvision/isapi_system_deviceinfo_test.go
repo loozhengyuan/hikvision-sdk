5bb4e7d8515883b0b8784049dccdc0ed8534723c
Loo Zheng Yuan <me@zhengyuan.sg>
2020-08-16 01:20:05 +0800

    add test and fixtures

---
+++ hikvision/isapi_system_deviceinfo_test.go
@@ -0,0 +1,96 @@
+package hikvision
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/loozhengyuan/hikvision-sdk/hikvision/resource"
+)
+
+func TestClient_GetDeviceInfo(t *testing.T) {
+	// Create test cases
+	cases := []struct {
+		name     string
+		encoding string
+		fixture  string
+	}{
+		{"default", "xml", "./testdata/fixtures/isapi_system_deviceinfo.xml"},
+	}
+
+	// Run test cases
+	for _, tc := range cases {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// Load fixtures
+			f, err := os.Open(tc.fixture)
+			if err != nil {
+				t.Fatalf("error loading test fixtures: %v", err)
+			}
+			defer f.Close()
+
+			// Read test fixture
+			b, err := ioutil.ReadAll(f)
+			if err != nil {
+				t.Fatalf("error reading file: %v", err)
+			}
+
+			// Derive content type
+			var contentType string
+			switch tc.encoding {
+			case "xml":
+				contentType = `application/xml; charset="UTF-8"`
+			case "json":
+				contentType = `application/json; charset="UTF-8"`
+			default:
+				t.Fatalf("error deriving content type, expect either of 'xml' or 'json' but got: %v", tc.encoding)
+			}
+
+			// Mock HTTP server
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", contentType)
+				w.WriteHeader(http.StatusOK)
+				w.Write(b)
+			})
+			server := httptest.NewServer(handler)
+
+			// Execute request
+			c, err := NewClient("localhost", "", "")
+			if err != nil {
+				t.Fatalf("error creating client: %v", err)
+			}
+			c.BaseURL = server.URL
+			got, err := c.GetDeviceInfo()
+			if err != nil {
+				t.Fatalf("error executing request: %v", err)
+			}
+
+			// Unmarshal test fixture
+			var want *resource.XMLDeviceInfo
+			switch tc.encoding {
+			case "xml":
+				if err := xml.Unmarshal(b, &want); err != nil {
+					t.Fatalf("error unmarshalling fixture: %v", err)
+				}
+			case "json":
+				if err := json.Unmarshal(b, &want); err != nil {
+					t.Fatalf("error unmarshalling fixture: %v", err)
+				}
+			default:
+				t.Fatalf("error deriving content type, expect either of 'xml' or 'json' but got: %v", tc.encoding)
+			}
+
+			// Assert struct
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("error struct not equal: got %+v want %+v", got, want)
+			}
+		})
+	}
+}
